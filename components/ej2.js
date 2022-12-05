16b6e96ff3969008bdb91492fb5f56a4e4f8c1e5
Sergio Matarredona <semare@floridauniversitaria.es>
2022-12-05 16:08:14 +0100

    com2

--- components/ej2.js
+++ components/ej2.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {useState } from 'react';
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { HelperText, TextInput} from 'react-native-paper';
+import { HelperText, TextInput } from 'react-native-paper';
 
 export const EntradaDEmail = () => {
     const [text, setText] = useState('');
-    // const [checked, setChecked] = useState(false);
+    const [checked, setChecked] = useState(false);
 
     const hasErrors = () => {
         const expReg = /^[a-zA-Z]+\@[a-zA-Z\_\-0-9]+\.[a-z]{2,5}$/;
-        return( expReg.test(text) ? "":"El Email introduit no es valid!");
+        return (expReg.test(text) ? "" : "El Email introduit no es valid!");
     };
 
     return (
@@ -18,7 +18,7 @@ export const EntradaDEmail = () => {
                 value={text}
                 keyboardType="email-address"
                 right={<TextInput.Icon icon="check" />}
-                onChangeText={text => setText(text)}/>
+                onChangeText={text => setText(text)} />
             <HelperText type="error" visible={hasErrors()}>
                 El Email introduit no es valid!
             </HelperText>
--- components/ej3.js
+++ components/ej3.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import {useState } from 'react';
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { HelperText, TextInput} from 'react-native-paper';
+import { HelperText, TextInput } from 'react-native-paper';
 
 export const EntradaDeTelefon = () => {
     const [text, setText] = useState('');
-    // const [checked, setChecked] = useState(false);
+    const [checked, setChecked] = useState(false);
 
     const hasErrors = () => {
         const expReg = /^[a-zA-Z]+\@[a-zA-Z\_\-0-9]+\.[a-z]{2,5}$/;
-        return( expReg.test(text) ? "":"El numero introduit no es valid!");
+        return (expReg.test(text) ? "" : "El numero introduit no es valid!");
     };
 
     return (
         <View>
             <TextInput label="Numero de telefon"
                 value={text}
+                keyboardType="numeric"
                 right={<TextInput.Icon icon="check" />}
-                onChangeText={text => setText(text)}/>
+                onChangeText={text => setText(text)} />
             <HelperText type="error" visible={hasErrors()}>
                 El Numero introduit no es valid!
             </HelperText>
